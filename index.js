6e2f4414a944c8836a934fa4ee837aca78b61f28
André Roy <andreroy25.9@gmail.com>
2020-08-05 22:54:11 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+//! QUERY SELECTORS
+const drumsButtons = document.querySelectorAll(".drum");
+
+//! EVENT LISTENERS
+drumsButtons.forEach((element) => {
+  element.addEventListener("click", () => {
+    playSound(element.innerHTML);
+    buttonAnimation(element.innerHTML);
+  });
+});
+
+document.addEventListener("keydown", (e) => {
+  playSound(e.key);
+  buttonAnimation(e.key);
+});
+
+//! GLOBAL FUNCTIONS
+function playSound(key) {
+  switch (key) {
+    case "w":
+      const tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      const tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      const tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      const tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      const snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      const crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      const kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+      console.log(key);
+      break;
+  }
+}
+
+function buttonAnimation(currentKey) {
+  const activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(() => {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
